ea36db2c6c3e0d55aeb28c36a493454ef8d9689f
Stan-Getz <stanislav.genunchi@gmail.com>
2021-06-18 00:03:23 +0200

    Update components

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -4,32 +4,32 @@ import StyledButton from '../StyledButton';
 import styles from './styles';
 
 const CarItem = (props) => {
+  const { name, tagline, image } = props;
+
   return (
     <View style={styles.carContainer}>
-      <ImageBackground
-        source={require('../../assets/images/ModelS.jpeg')}
-        style={styles.image}
-      />
+      <ImageBackground source={image} style={styles.image} />
       <View style={styles.titles}>
-        <Text style={styles.title}>Model S</Text>
-        <Text style={styles.subtitle}>Starting at €86,990</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>{tagline}</Text>
       </View>
 
-      <StyledButton
-        type="primary"
-        content={'custom order'}
-        onPress={() => {
-          console.warn('Custom Order was pressed');
-        }}
-      />
-
-      <StyledButton
-        type="secondary"
-        content={'existing inventory'}
-        onPress={() => {
-          console.warn('Existing Inventory was pressed');
-        }}
-      />
+      <View style={styles.buttonsContainer}>
+        <StyledButton
+          type="primary"
+          content={'custom order'}
+          onPress={() => {
+            console.warn('Custom Order was pressed');
+          }}
+        />
+        <StyledButton
+          type="secondary"
+          content={'existing inventory'}
+          onPress={() => {
+            console.warn('Existing Inventory was pressed');
+          }}
+        />
+      </View>
     </View>
   );
 };
